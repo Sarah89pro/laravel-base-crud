ac6bfda1ad7c36ad5fd9eb2229a17e9c342cd2fe
Sarah89Pro <sarahproietti1@gmail.com>
2021-06-14 16:30:19 +0200

    homepage,model, controller, table & seeder

---
+++ database/migrations/2021_06_14_125821_create_comics_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 50);
+            $table->string('slug');
+            $table->text('description');
+            $table->string('thumb');
+            $table->string('price', 10);
+            $table->text('series');
+            $table->date('sale_date');
+            $table->string('type', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="mt-5 text-center">
+    Comics &hearts; for Comics lovers!
+</footer>
